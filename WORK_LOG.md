61a3d3fd1e2dac23472ce7b1ecc30932d4ef95c3
ldhaliwal <liliana.dhaliwal@menloschool.org>
2024-10-30 13:45:18 -0700

    brainstorming and a bit of code?

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
-Name: YOUR NAME HERE
+Name: Liliana Dhaliwal
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date       |   Time   |                                                                                                                 Update |
+|:-----------|:--------:|-----------------------------------------------------------------------------------------------------------------------:|
+| October 25 | 2.15-3pm | brainstormed a lot. Thinking about ways to store the data, I think hashing is definitely going to be involved somehow. |
+| October 29 |  9-10pm  |                                                                   did more brainstorming, wrote code to read the file. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Finder.java
+++ src/Finder.java
@@ -6,23 +6,24 @@ import java.io.IOException;
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Liliana Dhaliwal
  **/
 
 public class Finder {
+    static final int RADIX = 256;
+    static final long PRIME = 2147483647L;
 
     private static final String INVALID = "INVALID KEY";
 
     public Finder() {}
 
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // TODO: Complete the buildTable() function!
         String line;
         while ((line = br.readLine()) != null) {
             String[] columns = line.split(",");
             if (columns.length > Math.max(keyCol, valCol)) {
-                String key = columns[keyCol].trim();
-                String value = columns[valCol].trim();
+                String key = columns[keyCol];
+                String value = columns[valCol];
                 // call something here that inserts the key and values into my made up data type
             }
         }
@@ -31,8 +32,17 @@ public class Finder {
 
     public String query(String key){
         // TODO: Complete the query() function!
+        long index = hash(key);
+
         return INVALID;
     }
 
-    // definitely going to need a hash function at some point
+    public static long hash(String text){
+        long hash = 0;
+
+        for(int i = 0; i < text.length(); i++) {
+            hash = (hash * RADIX + text.charAt(i)) % PRIME;
+        }
+        return hash;
+    }
 }
